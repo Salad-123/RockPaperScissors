551ca6f8340072cc4bc838df7b7dea01ad66c16c
Sola Rivett <sola.rivett1@gmail.com>
2023-04-22 12:43:03 +1000

    Add project files.

---
+++ RockPaperScissors/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("***Welcome to Rock Paper Scissors***");
+        Console.WriteLine("");
+    }
+}
