94e77ead03fba8deabfd4f42dab60042d8b62b1d
Sola Rivett <sola.rivett1@gmail.com>
2023-04-22 13:04:54 +1000

    Adding game title and inputting user names

--- RockPaperScissors/Program.cs
+++ RockPaperScissors/Program.cs
@@ -3,9 +3,21 @@ using System.IO;
 
 class Program
 {
-    public static void Main(string[] args)
+    string playerOne = "";
+    public static void Main (string[] args)
     {
+        //Displays game title
         Console.WriteLine("***Welcome to Rock Paper Scissors***");
-        Console.WriteLine("");
+        Console.WriteLine("Lets Begin");
+        Console.WriteLine(" ");
+        
+        //Asks users for name and saves to variables playerOne and playerTwo
+        Console.WriteLine("Player 1, enter your name and press enter: ");
+        string playerOne = Console.ReadLine();
+        Console.WriteLine("Player 2, enter your name and press enter: ");
+        string playerTwo = Console.ReadLine();
+        Console.WriteLine("Welcome "+playerOne+" and "+playerTwo);
+        Console.WriteLine("Press any key to continue...");
+        Console.ReadKey()
     }
 }
