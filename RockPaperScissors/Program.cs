6c566866704a97c217dacc2f59515b33aa66997d
Sola <sola.rivett1@gmail.com>
2023-04-26 16:01:11 +1000

    Fixing issues

--- RockPaperScissors/Program.cs
+++ RockPaperScissors/Program.cs
@@ -1,6 +1,5 @@
 ﻿using System;
-using System.Diagnostics.Metrics;
-using System.Diagnostics;
+
 using System.IO;
 
 class Program
@@ -10,22 +9,26 @@ class Program
     private static string playerOneMove = "";
     private static string playerTwoMove = "";
 
+
     public static void Main(string[] args)
     {
-        GameTitle();
-        //do while to repeat game depending on user choice
+        Console.ForegroundColor = ConsoleColor.Red;
+        // Do while loop to repeat game depending on user choice
         do
         {
-            //The Console.Writeline(" ");'s add blank space between the different functions to avoid clutter
+            // The Console.Writeline(" ");'s add blank space between the different functions to avoid clutter
+            // Running functions
+            GameTitle();
             Console.WriteLine(" ");
             playerOne = InputName(1);
             Console.WriteLine(" ");
             playerTwo = InputName(2);
             Console.WriteLine(" ");
+            // Greets the users once they enter their names
             Console.WriteLine($"Welcome {playerOne} and {playerTwo}!");
             Console.WriteLine("Press any key to continue...");
-            Console.ReadKey();
-            Console.WriteLine(" ");
+            // Allows users to continue by pressing any key
+            Console.ReadKey(true);
             Console.WriteLine(" ");
             playerOneMove = InputMove(playerOne);
             Console.WriteLine(" ");
@@ -44,14 +47,14 @@ class Program
         Console.WriteLine("Let's Begin");
     }
 
-    // Prompts the user to enter their name and returns the name as a string
+    // Asks the user to enter their name and returns the input
     public static string InputName(int number)
     {
         Console.WriteLine("Player " + number + " enter your name and press enter: ");
         return Console.ReadLine();
     }
 
-    // Prompts the user to enter their move asn "Rock", "Paper" or "Scissors" and returns the move as string
+    // Asks the user to enter their move as "Rock", "Paper" or "Scissors" and returns the input
     public static string InputMove(string name)
     {
 
@@ -61,19 +64,19 @@ class Program
         return Console.ReadLine();
     }
 
-    // Compares the moves of player one and player two and displays the result of the game
+    // Compares the moves of player one and player two and prints the result of the game
     public static void GameResult()
     {
-        if ((playerOneMove == "Rock" && playerTwoMove == "Scissors") ||
-            (playerOneMove == "Paper" && playerTwoMove == "Rock") ||
-            (playerOneMove == "Scissors" && playerTwoMove == "Paper"))
+        if ((playerOneMove == "Rock" && playerTwoMove == "Scissors") 
+            || (playerOneMove == "Paper" && playerTwoMove == "Rock") 
+            || (playerOneMove == "Scissors" && playerTwoMove == "Paper"))
         {
 
             Console.WriteLine($"{playerOne} wins! {playerOne}'s {playerOneMove} beats {playerTwo}'s {playerTwoMove}!");
         }
-        else if ((playerTwoMove == "Rock" && playerOneMove == "Scissors") ||
-                 (playerTwoMove == "Paper" && playerOneMove == "Rock") ||
-                 (playerTwoMove == "Scissors" && playerOneMove == "Paper"))
+        else if ((playerTwoMove == "Rock" && playerOneMove == "Scissors") 
+                || (playerTwoMove == "Paper" && playerOneMove == "Rock") 
+                || (playerTwoMove == "Scissors" && playerOneMove == "Paper"))
         {
             Console.WriteLine($"{playerTwo} wins! {playerTwo}'s {playerTwoMove} beats {playerOne}'s {playerOneMove}!");
         }
@@ -83,21 +86,20 @@ class Program
         }
         else
         {
-            Console.WriteLine("Invalid move chosen, please start the game over again");
+            Console.WriteLine("Invalid move chosen, please start the game over again.");
         }
     }
 
-    // Prompts the user to press "y" to play again or "n" to exit program
+    // Asks the user to press "y" to play again or "n" to exit program
     public static bool PlayAgain()
     {
-        Console.WriteLine("Thanks for playing Rock, Paper, Scissors!") ;
+        Console.WriteLine("Thanks for playing Rock, Paper, Scissors!");
         Console.WriteLine("Do you want to play again? (y/n)?");
-        string answer = Console.ReadLine().ToLower();
+        string answer = Console.ReadLine();
 
         if (answer == "y")
         {
             Console.Clear();
-            GameTitle();
             return true;
         }
         else if (answer == "n")
